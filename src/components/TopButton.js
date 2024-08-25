aca251b5120a64b05f00c06a82fcdf70c92f73a2
睿 <122418084+Rui01sir@users.noreply.github.com>
2024-08-26 05:30:50 +0800

    Update TopButton.js

--- src/components/TopButton.js
+++ src/components/TopButton.js
@@ -3,33 +3,33 @@ import React, { useState, useEffect } from 'react';
 function TopButton() {
     const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 300) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
-    };
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 300) {
+            setIsVisible(true);
+            } else {
+            setIsVisible(false);
+            }
+        };
 
     window.addEventListener('scroll', handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+            window.removeEventListener('scroll', handleScroll);
+        };
+        }, []);
 
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        };
 
 
     return(
         isVisible && (
             <button className="scroll-to-top" onClick={scrollToTop}>
-              <img src='./img/topbutton-icon.svg' alt='Top-button'></img>
+                <img src='./img/topbutton-icon.svg' alt='Top-button'></img>
             </button>
-          )
+            )
     )
 }
 
