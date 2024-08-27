eaeaa0883369fd6179f3b542a9fcea4872e9fc75
睿 <122418084+Rui01sir@users.noreply.github.com>
2024-08-27 18:54:48 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import './App.css';
 import 'animate.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Homepage from './components/Homepage';
+import HomePage from './components/HomePage';
 import Company from './components/Company';
 import Navbar from './components/Navbar';
 import Page404 from './components/Page404';
 import Footer from './components/Footer';
 import TopButton from './components/TopButton';
 import Contact from './components/Contact';
+import Product from './components/Product';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/Company" element={<Company />} />
         <Route path="/Contact" element={<Contact />}/>
+        <Route path="/Product" element={<Product />}/>
         <Route path="*" element={<Page404 />} />
       </Routes>
       <TopButton />
