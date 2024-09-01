81a618448fb3103da6d4a023281a4f2a0b406287
睿 <122418084+Rui01sir@users.noreply.github.com>
2024-09-02 06:03:48 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ import HomePage from './components/page/HomePage';
 import Company from './components/page/Company';
 import Contact from './components/page/Contact';
 import Product from './components/page/Product';
+import Support from './components/page/Support';
+import SupportList from './components/page/SupportList';
+import News from './components/page/News';
+import NewsDetail from './components/page/NewsDetail';
 import Page404 from './components/page/Page404';
 import TopButton from './components/common/TopButton';
 import Footer from './components/common/Footer';
 import Loading from './components/animations/Loading';
 
-
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+
   const handleLoadingFinish = () => {
     setIsLoading(false);
   };
@@ -27,6 +31,10 @@ function App() {
         <Route path="/Company" element={<Loading Component={Company} onLoadingFinish={handleLoadingFinish} />} />
         <Route path="/Contact" element={<Loading Component={Contact} onLoadingFinish={handleLoadingFinish} />} />
         <Route path="/Product" element={<Loading Component={Product} onLoadingFinish={handleLoadingFinish} />} />
+        <Route path="/Support" element={<Loading Component={Support} onLoadingFinish={handleLoadingFinish} />} />
+        <Route path="/list" element={<Loading Component={SupportList} onLoadingFinish={handleLoadingFinish} />} />
+        <Route path="/News" element={<Loading Component={News} onLoadingFinish={handleLoadingFinish} />} />
+        <Route path="/news/:id" element={<NewsDetail />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
       <TopButton />
